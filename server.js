6505e623cabe68590af194acffeb1a7b3950b0c1
catali73 <luiscatala@outlook.com>
2026-03-03 17:21:53 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(join(__dirname, 'dist')))
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`CCEE App running on port ${PORT}`)
+})
